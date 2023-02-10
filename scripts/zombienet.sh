3774b24d9a3bc24e0d25e220e11a963d21a1636a
rusty <dalegribble@riseup.net>
2023-02-10 14:05:11 -0800

    remove working dir path for zombienet config

--- scripts/zombienet.sh
+++ scripts/zombienet.sh
@@ -4,4 +4,4 @@
 echo "*** Starting zombienet..."
 
 ZOMB_WS_PORT=12345
-./zombienet spawn --provider native $(pwd)/rinzler_zombienet_config.toml
+./zombienet spawn --provider native rinzler_zombienet_config.toml
