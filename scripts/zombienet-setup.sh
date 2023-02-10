88ee3806244946f3617159c30849cd99138e1964
rusty <dalegribble@riseup.net>
2023-02-10 10:39:13 -0800

    Add md5 check + use working dir

--- scripts/zombienet-setup.sh
+++ scripts/zombienet-setup.sh
@@ -1,47 +1,68 @@
 #!/bin/bash
-# This script is meant to be run on Unix/Linux based systems
 
 echo "*** Initializing Zombienet installation"
 
+WORKING_DIR=$(pwd)
+BRANCH="origin/release-v0.9.32"
+
 # Download zombienet bin
 DIST="linux"
+CHECKSUM=""
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	ARCH=""
-    case $(uname -m) in
-		x86_64) ARCH="x64" ;;
-		arm)    dpkg --print-architecture | grep -q "arm64" && ARCH="arm64" || ARCH="arm64" ;;
+	case $(uname -m) in
+		x86_64) ARCH="x64"; CHECKSUM="d864e830850bdd10718fd3ec27aa6bdd" ;;
+		arm)    dpkg --print-architecture | grep -q "arm64" && ARCH="arm64" || ARCH="arm64"; CHECKSUM="c328b2eced5b21b313dc9af7558f5557" ;;
 	esac
 
 	DIST+="-${ARCH}"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    DIST="macos"
+	DIST="macos"
+	CHECKSUM="c4511801b40305c13e45edbd506961be"
 else
-    echo "*** Unknown distribution, stopping..."
+	echo "*** Unknown distribution, stopping..."
 	exit
 fi
 
-DIR="../zombienet"
+DIR="${WORKING_DIR}/zombienet"
 mkdir $DIR
 
 FILE_PATH="${DIR}/zombienet"
 
 DOWNLOAD="https://github.com/paritytech/zombienet/releases/download/v1.3.33/zombienet-${DIST}"
-wget $DOWNLOAD -O $FILE_PATH
+echo "*** Downloading zombienet..."
+wget -q $DOWNLOAD -O $FILE_PATH
+if [[ "$(md5sum ${FILE_PATH} | awk '{print $1}')" != $CHECKSUM ]]; then
+	echo "Zombienet binary checksum not valid, exiting."
+	exit
+fi
 chmod +rx $FILE_PATH
 
+echo "*** Installing substrate support libraries"
+
 # Install support libraries for compiling substrate binaries
-curl https://getsubstrate.io -sSf | bash -s -- --fast
+# verify md5
+curl https://getsubstrate.io -sSf > support_install.sh
+if [[ "$(md5sum support_install.sh | awk '{print $1}')" != "7296b9d45a89e973528c3ae31719ff08" ]]; then
+	echo "Substrate library script checksum not valid, exiting."
+	exit
+fi
+chmod +rx support_install.sh
+bash support_install.sh
+rm support_install.sh
+
+echo "*** Downloading relay chain"
 
 # Download relay chain
 mkdir "${DIR}/polkadot"
 git clone https://github.com/paritytech/polkadot.git "${DIR}/polkadot"
-git checkout release-v0.9.32
+git checkout $BRANCH
 cd "${DIR}/polkadot"
 
 # Build relay chain
 cargo build --release
 
-cd ../../ # Back to main directory
+cd $WORKING_DIR # Back to working directory
 
 # Build rinzler
 cargo build --release
