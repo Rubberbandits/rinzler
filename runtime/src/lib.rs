879801f8556d36214052087cd303a332dde0371a
Ala Shaabana <ala@bittensor.com>
2023-02-20 13:56:20 -0500

    Updated on_initialize weight

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -176,7 +176,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bittensor-parachain"),
 	impl_name: create_runtime_str!("bittensor-parachain"),
 	authoring_version: 1,
-	spec_version: 23,
+	spec_version: 1,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
