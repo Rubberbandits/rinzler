433d6cdcb904bbfe93197339da8cf9a0eb6ad92e
Ala Shaabana <ala@bittensor.com>
2023-02-21 20:30:58 -0500

    Added benchmark for sudo register

--- pallets/paratensor/src/benchmarks.rs
+++ pallets/paratensor/src/benchmarks.rs
@@ -4,14 +4,11 @@
 //mod benchmarking;
 
 use crate::*;
-use super::*;
 use crate::Pallet as Paratensor;
-use sp_runtime::traits::Bounded;
-use frame_benchmarking::{benchmarks, whitelisted_caller, impl_benchmark_test_suite, account};
+use frame_benchmarking::{benchmarks, whitelisted_caller, account};
 use frame_system::RawOrigin;
-use frame_support::sp_std::vec;
-use frame_support::inherent::Vec;
 pub use pallet::*;
+use frame_support::assert_ok;
 //use mock::{Test, new_test_ext};
 
 benchmarks! {
@@ -43,15 +40,25 @@ benchmarks! {
   benchmark_sudo_register {
     let caller: T::AccountId = whitelisted_caller::<AccountIdOf<T>>(); 
     let caller_origin = <T as frame_system::Config>::RuntimeOrigin::from(RawOrigin::Signed(caller.clone()));
-    Paratensor::<T>::create_network_with_weights(
-      caller_origin.clone(), 
-      11u16.into(), // netuid
-      4096u16.into(), // n
-      1000u16.into(), // tempo
-      100u16.into(), // n_vals
-      1000u16.into() // n_weights
-    );
-  }: sudo_register( RawOrigin::Signed( caller.clone() ),  11u16.into(), account("Alice", 0, 1), account("Bob", 0, 2), 100u64.into(), 120u64.into() )
+    let netuid: u16 = 1;
+    let tempo: u16 = 0;
+    let modality: u16 = 0;
+        let stake: u64 = 10;
+        let balance: u64 = 1000000000;
+
+    assert_ok!( Paratensor::<T>::do_add_network( RawOrigin::Root.into(), netuid.try_into().unwrap(), tempo.into(), modality.into()));
+    Paratensor::<T>::set_max_allowed_uids( netuid, 4096 ); 
+    assert_eq!(Paratensor::<T>::get_max_allowed_uids(netuid), 4096);
+
+    let mut seed : u32 = 1;
+    let block_number: u64 = Paratensor::<T>::get_current_block_as_u64();
+    let hotkey: T::AccountId = account("Alice", 0, seed);
+    let coldkey: T::AccountId = account("Test", 0, seed);
+
+    let amoun_to_be_staked = Paratensor::<T>::u64_to_balance( balance );
+    Paratensor::<T>::add_balance_to_coldkey_account(&coldkey.clone(), amoun_to_be_staked.unwrap());
+
+  }: sudo_register(RawOrigin::<AccountIdOf<T>>::Root, netuid, hotkey, coldkey, stake, balance)
   // Add individual benchmarks here
   // benchmark_drain_emission { 
 
